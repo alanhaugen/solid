0f3f4e1e2d50b3dc8058294279d681d0f7eac341
Alan <alanhaugen@gmail.com>
2023-05-13 19:14:14 +0200

    Cube works now...

--- source/core/components/cube.cpp
+++ source/core/components/cube.cpp
@@ -1,6 +1,77 @@
 #include "cube.h"
 
-Cube::Cube()
+Cube::Cube(float x, float y, float z, float length, float width, float height)
 {
+    matrix.position = glm::vec3(x, y, z);
 
+    Array<Pixmap*> *textures = NULL;
+    Array<String> shaders(2);
+    shaders[VERTEX_SHADER]   = filesystem->Open(URL("data/simple.vert"), PLAIN_TEXT)->Read();
+    shaders[FRAGMENT_SHADER] = filesystem->Open(URL("data/simple.frag"), PLAIN_TEXT)->Read();
+
+    Array<IDrawable::Vertex> vertices;
+    Array<unsigned int> indices;
+
+    // 36 coords for a cube
+    // Thanks to Dr. Anton https://antongerdelan.net/opengl/cubemaps.html
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height, -1.0f*width)));
+
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height,  1.0f*width)));
+
+    vertices.Add(IDrawable::Vertex(glm::vec3(1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(1.0f*length, -1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(1.0f*length, -1.0f*height, -1.0f*width)));
+
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length, -1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height,  1.0f*width)));
+
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length,  1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length,  1.0f*height, -1.0f*width)));
+
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length, -1.0f*height, -1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3(-1.0f*length, -1.0f*height,  1.0f*width)));
+    vertices.Add(IDrawable::Vertex(glm::vec3( 1.0f*length, -1.0f*height,  1.0f*width)));
+
+    drawable = renderer->CreateDrawable(vertices, indices, shaders, textures);
+    drawable->matrix = matrix.matrix;
+}
+
+Cube::~Cube()
+{
+    // TODO: delete cube from renderer
+}
+
+void Cube::Update()
+{
+    drawable->uniformData = uniforms;
+    drawable->matrix = matrix.subMatrix * matrix.matrix;
+    renderer->Draw(drawable);
+}
+
+void Cube::UpdateAfterPhysics()
+{
 }
--- source/core/components/cube.h
+++ source/core/components/cube.h
@@ -1,11 +1,19 @@
 #ifndef CUBE_H
 #define CUBE_H
 
+#include "core/components/component.h"
 
-class Cube
+class Cube : public IComponent
 {
+private:
+    IDrawable *drawable;
+
 public:
-    Cube();
+    Cube(float x = 0, float y = 0, float z = 0, float length = 1, float width = 1, float height = 1);
+    ~Cube();
+
+    void Update();
+    void UpdateAfterPhysics();
 };
 
 #endif // CUBE_H
