b763ae75cc4eca1fd966863efc36dd6004745f08
Alan <alanhaugen@gmail.com>
2023-08-20 11:22:18 +0200

    Added spinning cube example to test rotation. It works

---
+++ examples/primitives/source/main.cpp
@@ -0,0 +1,46 @@
+#include <core/application.h>
+#include <core/components/cube.h>
+#include <core/components/camera.h>
+
+class Primitives : public IScene
+{
+private:
+    Cube *cube;
+
+public:
+    Primitives();
+
+    void Init();
+    void Update();
+    void UpdateAfterPhysics();
+};
+
+Primitives::Primitives()
+{
+}
+
+void Primitives::Init()
+{
+    cube = new Cube(0.0f, 0.0f, -10.0f);
+
+    components.Add(new Camera());
+    components.Add(cube);
+}
+
+void Primitives::Update()
+{
+    cube->matrix.Rotate(0.1, glm::vec3(1.0f, 1.0f, 1.0f));
+}
+
+void Primitives::UpdateAfterPhysics()
+{
+}
+
+int main(int argc, char **argv)
+{
+    Application application(argc, argv);
+    application.AddScene(new Primitives());
+
+    return application.Exec();
+}
+
