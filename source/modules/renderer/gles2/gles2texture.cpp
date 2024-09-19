17341351bad46b819919c13fe8d9a150b43ee9b0
Alan <alanhaugen@gmail.com>
2024-09-19 17:10:00 +0200

    Changed texture to linear filtering

--- source/modules/renderer/gles2/gles2texture.cpp
+++ source/modules/renderer/gles2/gles2texture.cpp
@@ -87,8 +87,8 @@ void GLES2Texture::Load(String path, int type, GLenum sideTarget)
     {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         glBindTexture(GL_TEXTURE_2D, textureID);
 
