c49c3cb3a11b810d0d569d56a8d8a02c9bf7d544
Alan <alanhaugen@gmail.com>
2025-01-30 23:15:29 +0100

    A texture is now loaded but not uploaded to the gpu by vulkantexture

--- source/modules/renderer/vulkan/vulkantexture.cpp
+++ source/modules/renderer/vulkan/vulkantexture.cpp
@@ -1,4 +1,5 @@
 #include "vulkantexture.h"
+#include "core/x-platform/locator.h"
 
 //#define STBI_ONLY_PNG
 //#define STBI_NO_STDIO
@@ -11,6 +12,29 @@ VulkanTexture::VulkanTexture()
 
 VulkanTexture::VulkanTexture(String filePath)
 {
+    IFile *file;
+
+    if (filePath == "")
+    {
+        file = Locator::filesystem->Open(filePath);
+    }
+    else
+    {
+        file = Locator::filesystem->Open(filePath);
+    }
+
+    unsigned char *img = stbi_load_from_memory((const unsigned char*)file->Read(), file->Size(), &width, &height, &channels, 0);
+
+    if (channels == TEXTURE_CHANNELS::RGB)
+    {
+        //format = GL_RGB;
+    }
+    else
+    {
+        //format = GL_RGBA;
+    }
+
+    stbi_image_free(img);
 }
 
 VulkanTexture::VulkanTexture(String front, String back, String top, String bottom, String left, String right)
